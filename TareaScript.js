40ec869d516a8cedfcc743636a69bc8367982e5f
antoluz2005 <antonellaluzsambueza@gmail.com>
2022-10-05 17:34:19 +0200

    declarando fuction de suma

--- TareaScript.js
+++ TareaScript.js
@@ -5,3 +5,20 @@
 //--Generar un tipo de array y recorrerlo.--
 //--El proyecto se entrega por un repositorio en github/gitlab--
 
+var nombre= "antito";
+console.log (nombre);
+
+let deporte= 'patin';
+console.log (deporte);
+
+const color= `azul`;
+console.log (color);
+
+function suma (num1, num2){
+    var sumar= num1 + num2;
+    return sumar;
+}
+var num1= 3;
+var num2= 8;
+var sumar= suma(num1, num2);
+console.log (suma(num1,num2));
