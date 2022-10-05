313a2740513ff796aff5d066a5850ee54be4f276
antoluz2005 <antonellaluzsambueza@gmail.com>
2022-10-05 17:33:07 +0200

    declarando variables

---
+++ TareaScript.js
@@ -0,0 +1,7 @@
+//--Trabajo práctico individual--
+//--Generar un proyecto con js--
+//--Imprimir por consola los siguientes datos: variables, let, constantes.--
+//--Generar funciones de cálculos matemáticos--
+//--Generar un tipo de array y recorrerlo.--
+//--El proyecto se entrega por un repositorio en github/gitlab--
+
